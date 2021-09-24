7e52cd9e54bb47d60075cfa4b0132a4a32e293bb
Youngjae Lim <limyoungjae@gmail.com>
2021-09-24 00:31:19 -0400

    Domain implementation: Content & Hits

---
+++ src/lib/domain/clip/field/content.rs
@@ -0,0 +1,21 @@
+use super::ClipError;
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Clone, Deserialize, Serialize)]
+pub struct Content(String);
+
+impl Content {
+  pub fn new(content: &str) -> Result<Self, ClipError> {
+    if !content.trim().is_empty() {
+      Ok(Self(content.to_owned()))
+    } else {
+      Err(ClipError::EmptyContent)
+    }
+  }
+  pub fn into_inner(self) -> String {
+    self.0
+  }
+  pub fn as_str(&self) -> &str {
+    self.0.as_str()
+  }
+}
